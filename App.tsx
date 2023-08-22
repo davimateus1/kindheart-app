474074c8e2336f2a533043fcafee7f8ef2db020c
Davi Mateus <davi.2020109493@unicap.br>
2023-08-22 19:55:13 -0300

    setup: theme and first assets

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Text } from "native-base";
+import { theme } from "./src/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={theme}>
+      <Text color="brand.50">Test Custom Theme</Text>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/theme/index.ts
@@ -0,0 +1,18 @@
+import { extendTheme } from "native-base";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#28CD56",
+      100: "#1A1B23",
+      200: "#4A4A4A",
+    },
+  },
+  config: {
+    initialColorMode: "dark",
+  },
+  fonts: {
+    heading: "Montserrat",
+    body: "Poppins, sans-serif",
+  },
+});
